608a3c235310f17a3ec734dc0c1a95f36df1853d
Javeed Sanganakal <21953322+javeedsanganakal@users.noreply.github.com>
2022-10-14 18:54:42 -0700

    Update MinimumDominoRotationsForEqualRow.java

--- MinimumDominoRotationsForEqualRow.java
+++ MinimumDominoRotationsForEqualRow.java
@@ -47,3 +47,34 @@ class Solution {
         return Math.min(topRotation, bottomRotation);
     }
 }
+
+
+
+//Approach - 1: 2 Pass
+//Time Complexity : O(n)
+//Space Complexity : O(1) 
+ 
+class Solution {
+    public int minDominoRotations(int[] tops, int[] bottoms) {
+        
+        int n = tops.length;
+        //Element which you have to replcae
+        int candidate = -1;
+        int result = check(tops, bottoms, tops[0]);
+        if(result != -1) return result;
+        return check(tops, bottoms, bottoms[0]);
+    
+        
+    }
+    
+    private int check(int[] tops, int[] bottoms, int candidate){
+        int topRotation = 0;
+        int bottomRotation = 0;
+        for(int i=0; i<tops.length; i++){
+            if(candidate != tops[i] && candidate != bottoms[i]) return -1;
+            if(tops[i] != candidate) topRotation ++;
+            if(bottoms[i] != candidate) bottomRotation ++;
+        }
+        return Math.min(topRotation, bottomRotation);
+    }
+}
